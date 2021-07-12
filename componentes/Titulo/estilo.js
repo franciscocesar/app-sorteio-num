d3aaad64fa0c053b9147b472ab5f21d66715771c
franciscocesar <francisco.mateus@souunit.com.br>
2021-07-12 11:27:53 -0300

    App Sorteio V1.0

---
+++ componentes/Titulo/estilo.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const estilo = StyleSheet.create({
+  boxTitulo: {
+    height: 80,
+    backgroundColor: 'orange',
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    justifyContent: 'center'
+  },
+
+  titulo: {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 20
+  },
+  
+  subtitulo: {
+    color: '#fff'
+  },
+});
+
+export default estilo;
